a947f8d96eac8e52f1b5f51c5d79144b7dd3b6a1
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-21 00:19:12 -0400

    challenge complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -11,10 +11,44 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println("Loop " + i + " hello!");
         }
-        
+
+
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
+        for (int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
+        int firstNumber = 2;
+        int secondNumber = 1000;
+        int count = 0;
+        for (int i = firstNumber; i < secondNumber; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " is Prime");
+                count++;
+                if (count == 3) {
+                    break;
+                }
+            }
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return amount * (interestRate / 100);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
